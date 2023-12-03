15e4f7e1cbf48be624e533fc6af86ecae5905ba2
Gary Trovillion <geetro1@hotmail.com>
2023-12-03 16:11:53 -0600

    complete array filtering method lab

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
-// Code your solution here
+
+function findMatching(driversNames, name) {
+    const lowerCaseName = name.toLowerCase();
+    const result = driversNames.filter(driver => driver.toLowerCase() === name.toLowerCase());
+    return result;
+  }
+
+  function fuzzyMatch(driversNames, letter){
+    const result = driversNames.filter(driver => driver.substring(0,2) === letter)
+    return result
+}
+
+function matchName(driversNames, name){
+    const result = driversNames.filter(driver => driver.name === name)
+    return result
+}
